0858c2df518c0639f557a480c6bb88cd7c4acdb7
inphormatic <dvdabati@gmail.com>
2023-09-22 12:39:03 +0200

    Remove leftwm in favor of i3

--- setup.sh
+++ setup.sh
@@ -5,22 +5,18 @@ clear
 
 
 
-# Basic Packages
 declare -r INSTALLER=$(find_installer)
+declare -r DISTRO=$(find_distro_name)
 
-if [[ "$?" -ne 0 ]]; then
-  log 'error' 'It seems that these dotfiles are not yet available for this Linux distro. Aborting!'
-  exit 1
-fi
 
-declare -r PKGS='curl dunst feh nnn nodejs picom polybar ripgrep rofi scdoc tmux unzip zsh'
+# Basic Packages
+declare -r PKGS='curl dunst feh i3 nnn nodejs picom polybar ripgrep rofi scdoc tmux unzip zsh'
 
 log 'info' "Installing $PKGS ..."
-sudo $INSTALLER $PKGS > /dev/null
+sudo $INSTALLER $PKGS /dev/null
 
 
 # Developmet tools
-declare -r DISTRO=$(find_distro_name)
 declare -Ar DEV_TOOLS=(
   ["arch"]="base-devel"
   ["solus"]="-c system.devel"
@@ -28,13 +24,13 @@ declare -Ar DEV_TOOLS=(
 )
 
 log 'info' 'Installing development tools...'
-sudo $INSTALLER ${DEV_TOOLS[$DISTRO]} > /dev/null
+sudo $INSTALLER ${DEV_TOOLS[$DISTRO]}
 
 
 # Rust
 command -v rustup > /dev/null
 if [[ "$?" == 0 ]]; then
-  log 'warn' 'Rust is already istalled. Skipping!'
+  log 'info' 'Rust is already istalled. Skipping!'
 else
   log 'info' 'Installing Rust...'
   curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
@@ -45,7 +41,7 @@ fi
 # Go
 command -v go > /dev/null
 if [[ "$?" == 0 ]]; then
-  log 'warn' 'Go is already installed. Skipping!'
+  log 'info' 'Go is already installed. Skipping!'
 else
   log 'info' 'Downloading Go...'
   curl -sLO https://go.dev/dl/go1.21.1.linux-amd64.tar.gz
@@ -58,7 +54,9 @@ fi
 
 
 # Nerd Fonts
-if [[ ! -d '/usr/share/fonts/JetBrainsMono' ]]; then
+if [[ -d '/usr/share/fonts/JetBrainsMono' ]]; then
+  log 'info' 'JetBrainsMono already present into /usr/share/fonts. Skipping!'
+else
   log 'info' 'Downloading JetBrainsMono Nerd Fonts...'
   curl -sLO https://github.com/ryanoasis/nerd-fonts/releases/download/v3.0.2/JetBrainsMono.zip
 
@@ -68,15 +66,13 @@ if [[ ! -d '/usr/share/fonts/JetBrainsMono' ]]; then
 
   log 'info' 'Installing JetBrainsMono...'
   sudo mv JetBrainsMono /usr/share/fonts
-else
-  log 'warn' 'JetBrainsMono already present into /usr/share/fonts. Skipping!'
 fi
 
 
 # Neovim
 command -v nvim > /dev/null
 if [[ "$?" == 0 ]]; then
-  log 'warn' 'Neovim is already installed. Skipping!'
+  log 'info' 'Neovim is already installed. Skipping!'
 else
   log 'info' 'Installing Neovim...'
   curl -sLO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
@@ -86,92 +82,17 @@ else
 fi
 
 if [[ -d "$HOME/.config/nvim" ]]; then
-  log 'warn' 'A Neovim configuration is already present at ~/.config/nvim. Skipping!'
+  log 'info' 'A Neovim configuration is already present at ~/.config/nvim. Skipping!'
 else
   log 'info' 'Cloning Neovim configuration...'
   git clone https://github.com/inphormatic/dotfiles-neovim.git ~/.config/nvim
 fi
 
 
-# Leftwm
-command -v leftwm > /dev/null
-if [[ "$?" == 0 ]]; then
-  log 'warn' 'LeftWM is already present. Skipping!'
-else
-  log 'info' 'Installing LeftWM desktop env...'
-  log 'info' 'Cloning leftwm repo...'
-  git clone https://github.com/leftwm/leftwm.git ./leftwm-repo > /dev/null
-
-  log 'info' 'Building leftwm...'
-  cd leftwm-repo
-  cargo build --profile optimized > /dev/null
-  sudo install -s -Dm755 ./target/optimized/leftwm ./target/optimized/leftwm-worker ./target/optimized/lefthk-worker ./target/optimized/leftwm-state ./target/optimized/leftwm-check ./target/optimized/leftwm-command -t /usr/bin
-  sudo cp leftwm.desktop /usr/share/xsessions/
-
-  log 'success' 'LeftWM installed'
-  cd .. && rm -rf ./leftwm-repo
-fi
-
-# LeftWM config
-command -v leftwm-config > /dev/null
-if [[ "$?" == 0 ]]; then
-  log 'warn' 'LeftWM-Config already present in ~/.config. Skipping!'
-else
-  log 'info' 'Installing leftWM configuration...'
-  git clone https://github.com/leftwm/leftwm-config.git > /dev/null
-  cd ./leftwm-config
-  cargo build --release > /dev/null
-  sudo cp "$(pwd)"/target/release/leftwm-config /usr/bin/leftwm-config
-
-  log 'success' 'LeftWM-Config installed!'
-  cd .. && rm -rf leftwm-config
-
-  log 'info' 'Generating config.ini into ~/.config/leftwm ...'
-  leftwm-config -n
-fi
-
-# LeftWM theme
-command -v leftwm-theme > /dev/null
-if [[ "$?" == 0 ]]; then
-  log 'warn' 'LeftWM-Theme already installed.'
-else
-  log 'info' 'Installing leftwm-theme...'
-  git clone https://github.com/leftwm/leftwm-theme > /dev/null
-  cd leftwm-theme
-  cargo build --release > /dev/null
-
-  log 'success' 'Leftwm-theme installed!'
-  sudo install -s -Dm755 ./target/release/leftwm-theme -t /usr/bin
-  cd .. && rm -rf leftwm-theme
-fi
-
-if [[ -f "$HOME/.config/leftwm/themes.toml" ]]; then
-  log 'info' 'Themes already upgraded'
-else
-  log 'info' 'Generating themes.toml into ~/.config/leftwm...'
-  leftwm-theme update
-fi
-
-if [[ -d "$HOME/.config/leftwm/themes" ]]; then
-  log 'warn' 'A LeftWM theme already exists. Remove it if you want to apply a new one.'
-else
-  log 'info' 'Installing LeftWM Ascent theme...'
-  leftwm-theme install 'Ascent'
-  leftwm-theme apply 'Ascent'
-  
-  if [[ "$?" -ne 0 ]]; then
-    log 'error' 'Error during leftwm theme installation. Aborting!'
-    exit 1
-  fi
-
-  log 'success' 'LeftWM theme installed!'
-fi
-
-
 # Alacritty
 command -v alacritty > /dev/null
 if [[ "$?" == 0 ]]; then
-  log 'warn' 'Alacritty is already installed. Skipping!'
+  log 'info' 'Alacritty is already installed. Skipping!'
 else
   log 'info' 'Cloning Alacritty repo...'
   git clone https://github.com/alacritty/alacritty.git ./alacritty-repo > /dev/null
@@ -210,7 +131,7 @@ fi
 
 # Oh-My-Zsh
 if [[ -d "$HOME/.oh-my-zsh" ]]; then
-  log 'warn' 'Oh-My-Zsh is aleady installed. Skipping!'
+  log 'info' 'Oh-My-Zsh is aleady installed. Skipping!'
 else
   log 'info' 'Installing Oh-My-Zsh...'
   sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
@@ -219,7 +140,7 @@ fi
 
 # Dotfiles inclusion
 log 'info' 'Including dotfiles...'
-cp -TRa ./leftwm/. "$HOME/.config/leftwm"
+cp -TRa ./i3/. "$HOME/.config/i3"
 cp -TRa ./picom/. "$HOME/.config/picom"
 cp -TRa ./rofi/. "$HOME/.config/rofi"
 cp -TRa ./polybar/. "$HOME/.config/polybar"
