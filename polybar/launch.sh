d0d243d9330a35317cd7260f84db67c875e527bf
David <32371038+inphormatic@users.noreply.github.com>
2023-09-27 08:38:08 +0200

    Create launch.sh for polybar on all monitors

---
+++ polybar/launch.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar -c "$HOME/.config/polybar/polybar.ini" --reload mainbar &> /dev/null &
+  done
+else
+  polybar --reload example &
+fi
